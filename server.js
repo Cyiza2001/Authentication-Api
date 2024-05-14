63bc8d5e2fe3a171f47667bd49c6668ddbe8373d
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2024-05-14 15:28:26 +0200

    another commit

--- server.js
+++ server.js
@@ -14,18 +14,27 @@ const posts = [
   },
 ];
 app.get("/posts", authenticateToken, (req, res) => {
-  res.json(posts);
+  res.json(posts.filter((post) => post.username === req.user.name));
 });
 app.post("/login", (req, res) => {
   //Authenticate User
   const username = req.body.username;
+  console.log(username);
   const user = { name: username };
   const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
   res.json({ accessToken: accessToken });
 });
-function authenticateToken(req, res, nex) {
+
+function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
-  const token = authHeader.split(" ")[1];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
 }
 app.listen(3000, (req, res) => {
   //   res.Send("server is listenning on port 3000");
